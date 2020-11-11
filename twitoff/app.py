d198c0e3b400cf578ac6f06b8accd441f645bdee
Ben Somerville <70228881+bsmrvl@users.noreply.github.com>
2020-11-11 17:32:21 -0500

    fix env

--- twitoff/app.py
+++ twitoff/app.py
@@ -10,7 +10,7 @@ from .prediction import predict_user
 def create_app():
     app = Flask(__name__)
     app.secret_key = getenv('APP_KEY_SECRET')
-    app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
+    app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     DB.init_app(app)
 
